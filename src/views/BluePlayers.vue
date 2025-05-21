ca6921e8d46f7aa37a99e5e671269ac94bd7b8d2
hyeongjin-kim <49364688+hyeongjin-kim@users.noreply.github.com>
2025-05-21 09:34:17 +0900

    블루 챔피언 일러스트 크기 기준을 가로로 변경

--- src/views/BluePlayers.vue
+++ src/views/BluePlayers.vue
@@ -53,14 +53,14 @@
 <style scoped>
   .Blue_Player{
     display: flex;
-    width: auto;
-    height: 25vh;
+    width: 44vw;
+    height: auto;
     flex-wrap: nowrap;
   }
   .championiller{
     box-sizing: border-box;
-    width: auto;
-    height: 25vh;
+    width: 4.4vw;
+    height: auto;
   }
   .championiller.active{
     border-color: red;
